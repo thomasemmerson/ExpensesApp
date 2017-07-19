79c986eebd2d685c9303cec4788d6709f4b84251
Tom Emmerson <thomasemmerson@sky.com>
2017-07-19 14:01:08 +0100

    First commit

---
+++ ExpenseApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Approve? (Y/N)");
+            var response = Console.ReadLine();
+            if (response.ToUpper() == "Y")
+            {
+                Console.WriteLine("Approved");
+            } else
+            {
+                Console.WriteLine("Denied!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void TestFirstTest()
+        {
+            Assert.AreEqual(true, false);
+        }
+
+        [Test]
+        public void TestSecondTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
